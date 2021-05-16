3d647004be9765c9f8329dcb7da6e2e4fcbd453e
Gabriel Souza Silva <gabrielccpuc@gmail.com>
2021-05-16 10:23:17 -0300

    Config heroku

--- Api/index.js
+++ Api/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 
+const port = process.env.PORT || 4444;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(4444);
+app.listen(port);
--- Api/routes.js
+++ Api/routes.js
@@ -7,9 +7,7 @@ routes.get('/repositories',(req, res) =>{
     axios.get('https://api.github.com/orgs/takenet/repos')
     .then((response) => {
         var repositories =  response.data.filter(repository => repository.language == 'C#');
-
-        //console.log(repositories, typeof repositories)
-        console.log(repositories)
+        
         repositories.sort(
             function(a, b) {
                 var c = new Date(a.created_at);
@@ -18,18 +16,9 @@ routes.get('/repositories',(req, res) =>{
             }
         );
 
-        repositories = repositories.slice(0, 5)
+        repositories = repositories.slice(0, 5);
 
         res.json(repositories)
-
-        //res.json(response.data
-        //.find(value => value.language == 'C#')    
-        //.sort(
-        //    function(a,b){
-        //         return new Date(a.created_at) - new Date(b.created_at)
-        //     }
-        // )
-        // .filter((index) => index < 5))
     });
 });
 
